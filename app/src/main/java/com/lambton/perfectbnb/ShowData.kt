787fc1a75c646125eaa1f56ca43fd7b4023e3b16
000AGT744 <rimpykr@gmail.com>
2022-12-01 18:28:07 +0530

    added recyclerview

--- app/src/main/java/com/lambton/perfectbnb/ShowData.kt
+++ app/src/main/java/com/lambton/perfectbnb/ShowData.kt
@@ -1,6 +1,7 @@
 package com.lambton.perfectbnb
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,17 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.annotations.Nullable
+import kotlinx.android.synthetic.main.activity_admin_main.*
 import kotlinx.android.synthetic.main.header_layout.*
+
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,10 +28,13 @@ import retrofit2.Response
 
 class ShowData : Fragment() {
     var cityname: String? = ""
+    var updatedList=ArrayList<ItemviewModel>()
+    private lateinit var adapter: AdminMainAdapter
 
     val URL:String="https://api.openweathermap.org/data/2.5/weather?"
     var latitude:String="0.0"
     var longitude:String ="0.0"
+    lateinit var recyclerview:RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,6 +49,7 @@ class ShowData : Fragment() {
         cityname = arguments?.getString("city")
         latitude=arguments?.getString("lat").toString()
         longitude= arguments?.getString("lng").toString()
+        recyclerview=v.findViewById<RecyclerView>(R.id.recyclerview)
 
         requireActivity().setting.visibility=View.VISIBLE
         requireActivity().setting.setOnClickListener {
@@ -55,6 +70,7 @@ class ShowData : Fragment() {
             builder.show()
 
         }
+        initializeListView()
         return v
     }
 
@@ -75,7 +91,78 @@ class ShowData : Fragment() {
 
     }
 
+    fun initializeListView(){
 
+        recyclerview.visibility=View.VISIBLE
+
+        recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        adapter = object : AdminMainAdapter(requireContext(), updatedList) {
+            override fun itemClickListener(position: Int) {
+
+                Toast.makeText(requireContext(),"get click"+position,Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.reference
+        val firebaseAuth= FirebaseAuth.getInstance()
+
+        myRef.child("Places").addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(
+                snapshot: DataSnapshot,
+                @Nullable previousChildName: String?
+            ) {
+                // this method is called when new child is added to
+                // our data base and after adding new child
+                // we are adding that item inside our array list and
+                // notifying our adapter that the data in adapter is changed.
+                updatedList.add(snapshot.getValue(com.lambton.perfectbnb.ItemviewModel::class.java)!!)
+                Log.e("@#@","get list value"+updatedList.size+updatedList.get(0).Title)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildChanged(
+                snapshot: DataSnapshot,
+                @Nullable previousChildName: String?
+            ) {
+                // this method is called when the new child is added.
+                // when the new child is added to our list we will be
+                // notifying our adapter that data has changed.
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                // below method is called when we remove a child from our database.
+                // inside this method we are removing the child from our array list
+                // by comparing with it's value.
+                // after removing the data we are notifying our adapter that the
+                // data has been changed.
+                updatedList.remove(snapshot.getValue(com.lambton.perfectbnb.ItemviewModel::class.java))
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildMoved(
+                snapshot: DataSnapshot,
+                @Nullable previousChildName: String?
+            ) {
+                // this method is called when we move our
+                // child in our database.
+                // in our code we are note moving any child.
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // this method is called when we get any
+                // error from Firebase with error.
+            }
+        })
+        Log.e("@#@","get list"+updatedList.size)
+
+
+        recycler.adapter = adapter
+
+    }
 
     companion object {
         const val APIKEY="881ca47ef874c2b67cad0cfdd95b95e9"
